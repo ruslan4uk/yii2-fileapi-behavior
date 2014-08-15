fb0d1e83e14cb6e91b26433c43419b5a7ba7cbe3
Александр Выжанов <lembadm@gmail.com>
2014-08-15 16:53:58 +0300

    add Upload model

---
+++ models/Upload.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link http://www.astwell.com/
+ * @copyright Copyright (c) 2014 Astwell Soft
+ * @license http://www.astwell.com/license/
+ */
+
+namespace lembadm\fileapi\models;
+
+use yii\base\Model;
+
+/**
+ * Загрузочная модель файлов.
+ */
+class Upload extends Model
+{
+    /**
+     * Переменная используются для сбора пользовательской информации, но не сохраняются в базу.
+     * @var \yii\web\UploadedFile переданный файл/ы
+     */
+    public $file;
+}
